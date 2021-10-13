a6993680378e37ec0275fa0aaaad98538f0e62b1
Cherry Pie <Mr.CherryPieIn161@gmail.com>
2021-10-13 10:58:12 +0300

    Initial commit

---
+++ src/main/java/com/cherrypie/pieberry/PieberryApplication.java
@@ -0,0 +1,13 @@
+package com.cherrypie.pieberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieberryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieberryApplication.class, args);
+	}
+
+}
